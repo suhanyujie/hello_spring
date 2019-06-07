d47c9e9a12658f5c63baf61eca6c71eccfc3deea
suhanyujie <suhanyujie@qq.com>
2019-06-07 23:37:57 +0800

    从spring 容器中 获取对象

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,27 @@
 package hello;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         System.out.println("application");
         // 创建打印机
-        MessagePrinter printer = new MessagePrinter();
-        MessageService service = new MessageService();
+//        MessagePrinter printer = new MessagePrinter();
+//        MessageService service = new MessageService();
+//        printer.setMessageService(service);
+//        printer.printMessage();
+
+        // 初始化 spring 容器
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        // 获取对象
+        MessageService service = context.getBean(MessageService.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        System.out.println(printer);
+        System.out.println(service);
         printer.setMessageService(service);
         printer.printMessage();
     }
